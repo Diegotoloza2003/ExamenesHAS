639c14833070ee148fe0e466c9a076236f734635
Diegotoloza2003 <tolozad556@gmail.com>
2023-11-24 11:33:52 -0500

    first commit

---
+++ models/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+	//Datos de conexion con servidor
+
+	define("DB", "db_aa194e_examen");
+	define("HOST", "mysql5026.site4now.net");
+	define("USER", "aa194e_examen");
+	define("PASSWORD", "Holaquehace123");
+
+	const SHOW_ERRORS = false;
+
+?>
